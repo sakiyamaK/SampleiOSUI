1d2117ff7c76830023e19e2eeb0cf3afd707c2fe
sakiyamaK <sakiyama.k@gmail.com>
2023-12-10 02:45:42 +0900

    cellregistration完全に理解した

--- Modules/MainModule/Sources/CollectionViewFeature/ViewController/Trend/Sample/CellRegistration03ViewController.swift
+++ Modules/MainModule/Sources/CollectionViewFeature/ViewController/Trend/Sample/CellRegistration03ViewController.swift
@@ -5,44 +5,46 @@
 ////  Created by sakiyamaK on 2023/03/10.
 ////
 
-// カスタムセルなし番がまだ出来てない
-
 import DeclarativeUIKit
 import UIKit
 import Extensions
 
 struct Item: Hashable {
+
+    let uuid = UUID()
+
+    let id: Int
     let name: String
     let imageName: String
     let value: Int
     var isExpanded: Bool = false
-    
-    let id = UUID()
-    
+        
     static let all: [Item] = [
-        .init(name: "山田", imageName: "square.and.arrow.up", value: 1),
-        .init(name: "田中", imageName: "square.and.arrow.up.circle.fill", value: 222),
-        .init(name: "佐藤", imageName: "pencil.circle", value: 3333),
-        .init(name: "山田", imageName: "square.and.arrow.up", value: 1),
-        .init(name: "田中", imageName: "square.and.arrow.up.circle.fill", value: 222),
-        .init(name: "佐藤", imageName: "pencil.circle", value: 3333),
-        .init(name: "山田", imageName: "square.and.arrow.up", value: 1),
-        .init(name: "田中", imageName: "square.and.arrow.up.circle.fill", value: 222),
-        .init(name: "佐藤", imageName: "pencil.circle", value: 3333),
+        .init(id: 1, name: "山田", imageName: "square.and.arrow.up", value: 1),
+        .init(id: 2, name: "田中", imageName: "square.and.arrow.up.circle.fill", value: 222),
+        .init(id: 3, name: "佐藤", imageName: "pencil.circle", value: 3333),
+        .init(id: 1, name: "山田", imageName: "square.and.arrow.up", value: 1),
+        .init(id: 2, name: "田中", imageName: "square.and.arrow.up.circle.fill", value: 222),
+        .init(id: 3, name: "佐藤", imageName: "pencil.circle", value: 3333),
+        .init(id: 1, name: "山田", imageName: "square.and.arrow.up", value: 1),
+        .init(id: 2, name: "田中", imageName: "square.and.arrow.up.circle.fill", value: 222),
+        .init(id: 3, name: "佐藤", imageName: "pencil.circle", value: 3333),
     ]
 }
 
 struct CustomContentConfiguration: UIContentConfiguration, Hashable {
-    var hashValue: Int { item.id.hashValue }
+    var hashValue: Int { uuid.hashValue }
     func hash(into hasher: inout Hasher) {
         hasher.combine(hashValue)
     }
     static func == (lhs: CustomContentConfiguration, rhs: CustomContentConfiguration) -> Bool {
-        lhs.item.id == rhs.item.id
+        lhs.uuid == rhs.uuid
     }
     
-    var item: Item
-    var tapButton: (() -> Void)?
+    let uuid: UUID = .init()
+
+    let item: Item
+    let tapButton: (() -> Void)?
 
     func makeContentView() -> UIView & UIContentView {
         CustomContentView(configuration: self)
@@ -53,7 +55,7 @@ struct CustomContentConfiguration: UIContentConfiguration, Hashable {
     }
 }
 
-class CustomContentView: UIView, UIContentView {
+class CustomContentView: UIView {
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
@@ -62,17 +64,10 @@ class CustomContentView: UIView, UIContentView {
     private weak var showMore: UIButton!
     private weak var nameLabel: UILabel!
     private weak var descLabel: UILabel!
-        
-    var configuration: UIContentConfiguration {
-        get { appliedConfiguration }
-        set {
-            guard let newConfig = newValue as? CustomContentConfiguration else { return }
-            apply(configuration: newConfig)
-        }
-    }
-
-    init(configuration: CustomContentConfiguration) {
-        super.init(frame: .zero)
+    
+    var tapButton: (() -> Void)?
+    
+    private func setupLayout() {
         self.declarative(priorities: .init(bottom: .defaultHigh)) {
             UIStackView.vertical {
                 UIStackView.horizontal {
@@ -94,8 +89,8 @@ class CustomContentView: UIView, UIContentView {
                         UIButton("続きを読む")
                             .assign(to: &showMore)
                             .titleColor(.black)
-                            .addAction(.touchUpInside, handler: { _ in
-                                self.appliedConfiguration.tapButton?()
+                            .addAction(.touchUpInside, handler: {[weak self] _ in
+                                self?.tapButton?()
                             })
                             .right()
                         
@@ -106,40 +101,91 @@ class CustomContentView: UIView, UIContentView {
                 .padding(insets: .init(all: 20))
                 UIView.divider()
             }
-        }.apply(configuration: configuration)
+        }
+    }
+    
+    // 普通のUIViewとして使う時に必要な実装
+    init(item: Item, tapButton: (() -> Void)? = nil) {
+        super.init(frame: .zero)
+        setupLayout()
+        apply(item: item)
+        self.tapButton = tapButton
+    }
+    
+    func apply(item: Item) {
+        imageView.image = UIImage(systemName: item.imageName)
+        nameLabel.text = item.name
+        descLabel.text = Array(repeating: item.value.description, count: 100).joined()
+        descLabel.numberOfLines = item.isExpanded ? 0 : 1
+        showMore.title(item.isExpanded ? "閉じる" : "続きを読む")
+    }
+     
+    /***
+     UIContentViewに必要なパラメータ
+     */
+    private var appliedConfiguration: CustomContentConfiguration!
+}
+
+extension CustomContentView: UIContentView {
+    // UIContentViewのprotocolとして実装がいる
+    var configuration: UIContentConfiguration {
+        get { appliedConfiguration }
+        set {
+            guard let newConfig = newValue as? CustomContentConfiguration else { return }
+            apply(configuration: newConfig)
+        }
+    }
+    
+    convenience init(configuration: CustomContentConfiguration) {
+        self.init(item: configuration.item, tapButton: configuration.tapButton)
+        appliedConfiguration = configuration
     }
             
-    private var appliedConfiguration: CustomContentConfiguration!
-    
     private func apply(configuration: CustomContentConfiguration) {
         guard appliedConfiguration != configuration else { return }
         appliedConfiguration = configuration
-                
-        imageView.image = UIImage(systemName: configuration.item.imageName)
-        nameLabel.text = configuration.item.name
-        descLabel.text = Array(repeating: configuration.item.value.description, count: 100).joined()
-        descLabel.numberOfLines = configuration.item.isExpanded ? 0 : 1
-        showMore.isHidden = configuration.item.isExpanded
+        apply(item: configuration.item)
+        tapButton = appliedConfiguration.tapButton
     }
 }
 
 public class CellRegistration03ViewController: UIViewController {
     
-    private var items = Item.all
+    deinit { DLog() }
     
+    private var itemsInStackView = Item.all.first!
+    private var items = Item.all
+
     private var dataSource: UICollectionViewDiffableDataSource<Int, Item>!
-    private var collectionView: UICollectionView!
+
+    private weak var collectionView: UICollectionView!
+    private weak var customContentView: CustomContentView!
     
     public override func viewDidLoad() {
         super.viewDidLoad()
-        navigationItem.title = "Custom Configurations"
+
         self.applyView({
             $0.backgroundColor(.systemBackground)
+        }).applyNavigationItem({
+            $0.title = "Custom Configurations"
         }).declarative {
             UIStackView.vertical {
+                
+                UILabel("stackviewの中でも使える")
+                    .font(UIFont.defaultFontBold(size: 20))
+                    .padding(insets: .init(all: 8))
+                
+                CustomContentView(item: itemsInStackView, tapButton: {[weak self] in
+                    guard let self else { return }
+                    self.itemsInStackView.isExpanded.toggle()
+                    self.customContentView.apply(item: self.itemsInStackView)
+                })
+                .assign(to: &customContentView)
+                
                 UILabel("collectionviewの中でも使える")
                     .font(UIFont.defaultFontBold(size: 20))
                     .padding(insets: .init(all: 8))
+                
                 UICollectionView {
                     UICollectionViewCompositionalLayout.list(using: .init(appearance: .insetGrouped))
                 }
@@ -149,22 +195,20 @@ public class CellRegistration03ViewController: UIViewController {
     }
         
     private func configureDataSource() {
-        
-        let cellRegistration = UICollectionView.CellRegistration<UICollectionViewCell, Item> { (cell, indexPath, item) in
-            var content = CustomContentConfiguration(item: item)
-            content.tapButton = {[weak self] in
+        // dequeueConfiguredReusableCellでその都度生成はできないのでちゃんと定数化しておく
+        let cellRegistration = UICollectionView.CellRegistration<UICollectionViewCell, Item> { [weak self] (cell, indexPath, item) in
+            cell.contentConfiguration = CustomContentConfiguration(item: item, tapButton: {[weak self] in
                 guard let self else { return }
                 var updateItem = item
                 updateItem.isExpanded.toggle()
-                self.items = self.items.compactMap { i in
-                    i.id == updateItem.id ? updateItem : i
+                self.items = self.items.compactMap { item in
+                    item.uuid == updateItem.uuid ? updateItem : item
                 }
                 var snapshot = NSDiffableDataSourceSnapshot<Int, Item>()
                 snapshot.appendSections([0])
                 snapshot.appendItems(self.items)
                 self.dataSource.apply(snapshot, animatingDifferences: true)
-            }
-            cell.contentConfiguration = content
+            })
         }
         
         dataSource = UICollectionViewDiffableDataSource<Int, Item>(collectionView: collectionView) {
